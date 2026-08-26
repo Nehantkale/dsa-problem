83c1481d691b227193027861732ff2565429558e
Nehant kale <nehantkale4@gmail.com>
2026-08-26 20:00:07 +0530

    Time: 9 ms (56.66%), Space: 48.9 MB (40.39%) - CodeHub

---
+++ leetcode/0324-wiggle-sort-ii/0324-wiggle-sort-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void wiggleSort(int[] nums) {
+    int temp[]=nums.clone();
+    Arrays.sort(temp);
+      int n = nums.length;
+       int left = (n - 1) / 2;
+        int right = n - 1;
+         for (int i = 0; i < n; i++){
+            if (i % 2 == 0) {
+                nums[i] = temp[left--];
+            } else {
+                nums[i] = temp[right--];
+            }
+        }
+
+} }
