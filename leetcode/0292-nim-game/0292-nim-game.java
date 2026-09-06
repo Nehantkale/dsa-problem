a06e9c1d7daf92be18b917da4976f18f32554a9d
Nehant kale <nehantkale4@gmail.com>
2026-09-07 00:06:24 +0530

    Time: 0 ms (100%), Space: 42 MB (54.65%) - CodeHub

--- leetcode/0292-nim-game/0292-nim-game.java
+++ leetcode/0292-nim-game/0292-nim-game.java
@@ -1,12 +1,6 @@
 class Solution {
     public boolean canWinNim(int n) {
 
-        int t=n%4;
-        //int d=t%4;
-        if(t==0){
-            return false;
-        }
-        return true;
-        
+        return (n%4)!=0;
     }
-}
+    }
