b84cc4fc64972e7873a3380ed618767aba9d5159
Nehant kale <nehantkale4@gmail.com>
2026-09-07 18:50:43 +0530

    Add CodeChef LJAAS15 - Select one of the following options:

---
+++ codechef/LJAAS15 - Select one of the following options/solution.cpp
@@ -0,0 +1,11 @@
+System.out.println("JavaProgramming");: This option would print "JavaProgramming" without any space between "Java" and "Programming".
+
+System.out.println("Java " + "Programming");: This option correctly uses string concatenation with the plus operator to print "Java" and "Programming" with a space in between.
+
+System.out.println("Java" + "Programming");: Similar to the first option, this option would print "JavaProgramming" without any space between "Java" and "Programming".
+
+System.out.println("Java" + "
+" + "Programming");: This option would print "Java" and "Programming" on separate lines due to the newline character "
+" between them.
+
+So, the correct option to print "Java" and "Programming" on the same line with a space in between is option 2: System.out.println("Java " + "Programming");.
