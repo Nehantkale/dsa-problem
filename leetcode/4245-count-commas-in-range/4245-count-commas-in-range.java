06b82d011c1860ec0bff1629ea49e6654c1c8d86
Nehant kale <nehantkale4@gmail.com>
2026-09-08 19:42:13 +0530

    Time: 1 ms (83.65%), Space: 42.1 MB (98.83%) - CodeHub

---
+++ leetcode/4245-count-commas-in-range/4245-count-commas-in-range.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countCommas(int n) {
+        int num=0;
+        if(n<1000){
+            return 0;
+        }
+        else{
+             num=n-999;
+
+        }
+        return num;
+    }
+}
