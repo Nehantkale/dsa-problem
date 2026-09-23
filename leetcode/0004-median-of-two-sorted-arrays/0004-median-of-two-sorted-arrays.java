d3b6f470afb60c0b7945f1dd345e25fd6f861548
Nehant kale <nehantkale4@gmail.com>
2026-09-23 16:11:10 +0530

    Time: 1 ms (100%), Space: 49.2 MB (15.43%) - CodeHub

---
+++ leetcode/0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -0,0 +1,54 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+        // Always binary search the smaller array
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int m = nums1.length;
+        int n = nums2.length;
+
+        int left = 0;
+        int right = m;
+
+        while (left <= right) {
+
+            int i = (left + right) / 2;
+            int j = (m + n + 1) / 2 - i;
+
+            int left1 = (i == 0) ? Integer.MIN_VALUE : nums1[i - 1];
+            int right1 = (i == m) ? Integer.MAX_VALUE : nums1[i];
+
+            int left2 = (j == 0) ? Integer.MIN_VALUE : nums2[j - 1];
+            int right2 = (j == n) ? Integer.MAX_VALUE : nums2[j];
+
+            // Correct partition
+            if (left1 <= right2 && left2 <= right1) {
+
+                // Odd total length
+                if ((m + n) % 2 == 1) {
+                    return Math.max(left1, left2);
+                }
+
+                // Even total length
+                int maxLeft = Math.max(left1, left2);
+                int minRight = Math.min(right1, right2);
+
+                return (maxLeft + minRight) / 2.0;
+            }
+
+            // We moved too far in nums1
+            else if (left1 > right2) {
+                right = i - 1;
+            }
+
+            // We haven't moved far enough in nums1
+            else {
+                left = i + 1;
+            }
+        }
+
+        return 0.0;
+    }
+}
